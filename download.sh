e3e8237614413f4c2bc3de1850f7ccb6dda6b72c
frenchlam <frenchlam@gmail.com>
2017-02-21 09:49:36 +0100

    Modify database on the fly

--- download.sh
+++ download.sh
@@ -4,6 +4,7 @@ set -e -x;
 export Data_DIR=$(pwd)"/data"
 export START="2007"
 export END="2008"
+export DATABASE="airline_ontime"
 
 
 mkdir -p $Data_DIR
@@ -35,24 +36,33 @@ echo "Carrier, airport, plane-data : OK"
 #create table structure
 cd $Data_DIR
 
+tail -n+3 ../ddl/airline_create.sql > ddl/tmp_file_create ; mv ddl/tmp_file_create ddl/airline_create.sql
+sed -i "use $DATABASE;" ../ddl/to_orc.sql #1st line
+sed -i "create database if not exists $DATABASE;" ../ddl/to_orc.sql #1st line
+
 hive -v -f ../ddl/airline_create.sql
 echo "structure created"
 
+
+###### load data
+#create sql load file 
 LOAD_DATA_FILE="load_data_text.sql"
 
 rm -f ../ddl/$LOAD_DATA_FILE
 touch ../ddl/$LOAD_DATA_FILE
 
-echo "LOAD DATA LOCAL INPATH '$Data_DIR/carriers.csv.gz' INTO TABLE airline_ontime.airlines_raw;" >> ../ddl/$LOAD_DATA_FILE
-echo "LOAD DATA LOCAL INPATH '$Data_DIR/airports.csv.gz' INTO TABLE airline_ontime.airports_raw;" >> ../ddl/$LOAD_DATA_FILE
-echo "LOAD DATA LOCAL INPATH '$Data_DIR/plane-data.csv.gz' INTO TABLE airline_ontime.planes_raw;" >> ../ddl/$LOAD_DATA_FILE
+echo "LOAD DATA LOCAL INPATH '$Data_DIR/carriers.csv.gz' INTO TABLE $DATABASE.airlines_raw;" >> ../ddl/$LOAD_DATA_FILE
+echo "LOAD DATA LOCAL INPATH '$Data_DIR/airports.csv.gz' INTO TABLE $DATABASE.airports_raw;" >> ../ddl/$LOAD_DATA_FILE
+echo "LOAD DATA LOCAL INPATH '$Data_DIR/plane-data.csv.gz' INTO TABLE $DATABASE.planes_raw;" >> ../ddl/$LOAD_DATA_FILE
 
 
 for YEAR in $( seq $START $END )
 do
-	echo "LOAD DATA LOCAL INPATH '$Data_DIR/$YEAR.csv.bz2' INTO TABLE airline_ontime.flights_raw ;" >> ../ddl/$LOAD_DATA_FILE
+	echo "LOAD DATA LOCAL INPATH '$Data_DIR/$YEAR.csv.bz2' INTO TABLE $DATABASE.flights_raw ;" >> ../ddl/$LOAD_DATA_FILE
 done
 
+
+#load data 
 echo "loading data"
 hive -v -f ../ddl/$LOAD_DATA_FILE
 
--- optimize.sh
+++ optimize.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e -x;
+export DATABASE="airline_ontime"
 
+#Define the database 
+tail -n+2 ddl/to_orc.sql > ddl/tmp_file_orc ; mv ddl/tmp_file_orc ddl/to_orc.sql
+sed -i "use $DATABASE;" ../ddl/to_orc.sql
+
+tail -n+2 ddl/optimize.sql > ddl/tmp_file_op ; mv ddl/tmp_file_opt ddl/optimize.sql
+sed -i "use $DATABASE;" ../ddl/optimize.sql
+
+#execute the scripts
 echo "to ORC"
 hive -v -f ddl/to_orc.sql
 
